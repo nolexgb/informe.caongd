49c2628a2cf3c7b264b9f84e956be202216384ee
nolexgb <nolan.gb83@gmail.com>
2026-04-17 10:42:02 +0200

    Delete src/layout directory

--- src/layout/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <div>CAONGD · Demo V3 lista para GitHub</div>
-      <div>React · Vite · Leaflet · Filtros por URL</div>
-    </footer>
-  )
-}
