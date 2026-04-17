7188d28c5b238d9a143fcba7752f747d8598927d
nolexgb <nolan.gb83@gmail.com>
2026-04-17 10:09:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { DataProvider } from './context/DataContext'
-import { FilterProvider } from './context/FilterContext'
-import PageShell from './layout/PageShell'
-import HomePage from './pages/HomePage'
-import AndaluciaPage from './pages/AndaluciaPage'
-import OtrosPaisesPage from './pages/OtrosPaisesPage'
-import BaseSocialPage from './pages/BaseSocialPage'
-import ONGDPage from './pages/ONGDPage'
-import ComparePage from './pages/ComparePage'
-
 export default function App() {
   return (
-    <DataProvider>
-      <FilterProvider>
-        <PageShell>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/andalucia" element={<AndaluciaPage />} />
-            <Route path="/otros-paises" element={<OtrosPaisesPage />} />
-            <Route path="/base-social" element={<BaseSocialPage />} />
-            <Route path="/ongd" element={<ONGDPage />} />
-            <Route path="/comparador" element={<ComparePage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </PageShell>
-      </FilterProvider>
-    </DataProvider>
+    <div style={{
+      minHeight: '100vh',
+      background: '#08111b',
+      color: 'white',
+      fontFamily: 'Inter, Arial, sans-serif',
+      padding: '40px'
+    }}>
+      <h1>CAONGD Data Explorer V3</h1>
+      <p>La aplicación ya está desplegada correctamente en GitHub Pages.</p>
+    </div>
   )
 }
