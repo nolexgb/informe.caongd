76bb5b42cfbf0ca1a9ce90b53b2fa8fdeb47a8e1
nolexgb <nolan.gb83@gmail.com>
2026-04-27 17:11:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// src/main.jsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
+// ORDEN CORRECTO DE ESTILOS
 import "./styles/global.css";
 import "leaflet/dist/leaflet.css";
+import "./styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
